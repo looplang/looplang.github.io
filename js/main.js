0e96ad3cb17d3ecf9e688607f100dcc6cb44f590
Dhanji R. Prasanna <dhanji@gmail.com>
2012-04-15 17:58:54 +0800

    Developed a bunch of examples and styles. Done with front page. Next up: docs

--- js/main.js
+++ js/main.js
@@ -1,15 +1,113 @@
 /**
  * looplang.org: by Dhanji R. Prasanna (dhanji at gmail)
  */
+
+var OPERATORS = ['->', '<-', '=>', 'and', 'or', 'not', 'class', 'if', 'then', 'else',
+  'where', 'for', 'in', '==', '>=', '<=', 'true', 'false'];
+
+(function() {
+  var ops = OPERATORS;
+  OPERATORS = {};
+  for (var k = 0; k < ops.length; k++) {
+    OPERATORS[ops[k]] = true;
+  }
+})();
+
+ESC = 27;
+LEFT = 37;
+RIGHT = 39;
+
+function escape(token) {
+  return $('<div/>').text(token).html();
+}
+
 $(function() {
-  $('div.code > pre').each(function() {
+  $('div.code pre').each(function() {
     var code = $(this).text();
 
     var html = [];
     // Process into code markup...
+    var tokens = code.split(/([., \r\n\(\){}\[\]]+)/);
 
+    var inString = false, inComment = false;
+    for (var i = 0; i < tokens.length; i++) {
+      var token = tokens[i];
+
+      var trimmedToken = $.trim(token);
+      if (trimmedToken == '') {
+        // Special case for handling single-line comments
+        if (!inString && token.match(/.*[\n\r].*/)) {
+          inComment = false;
+          token = '</span>' + token;
+        }
+        html.push(token);
+        continue;
+      }
+
+      if (!inComment && token.match(/^["'`]/)) {
+        inString = true;
+        token = '<span class="string">' + token
+      }
+
+      if (!inComment && token.match(/["'`]$/)) {
+        inString = false;
+        token += '</span>';
+      } else if (!inString && trimmedToken == '#') {
+        inComment = true;
+        token = '<span class="comment">' + token;
+      } else if (!inString && token.match(/.*[\n\r].*/)) {
+        inComment = false;
+        token = '</span>' + token;
+      } else if (!inString) {
+        // highlight the rest...
+        if (OPERATORS[trimmedToken])
+          token = '<span class="operator">' + escape(token) + '</span>';
+        else if (trimmedToken.charAt(0).match(/[A-Z]/))
+          token = '<span class="typeLiteral">' + escape(token) + '</span>';
+        else if (trimmedToken.match(/[0-9]+/))
+          token = '<span class="numeric">' + escape(token) + '</span>';
+        else if (trimmedToken.charAt(0) == '@')
+          token = '<span class="symbol">' + escape(token) + '</span>';
+      }
+
+      html.push(token);
+    }
 
     html = html.join('');
-    $(this).html(code);
+
+    $(this).html(html);
+  });
+
+  var spin = function(forward) {
+    var target = $('#scriptTitle');
+    var parent = target.parent();
+    var visibleLi = parent.find('ol > li.on');
+    var next = forward ? visibleLi.next() : visibleLi.prev();
+
+    // Loop back to the first.
+    if (!next.length) {
+      var list = parent.find('ol > li');
+      next = forward ? list.first() : list.last();
+    }
+
+    visibleLi.removeClass('on');
+    next.addClass('on');
+
+    // Update the text in our link.
+    target.find('span').html(next.attr('title'));
+    parent[0].style['webkitTransitionDuration'] = '500';
+
+    return false;
+  };
+
+  // Setup spinner.
+  $('#scriptTitle').click(function() {
+    spin(true);
+  });
+
+  // Arrow keys to move backwards.
+  $(document).keydown(function(event) {
+    if (event.which == LEFT || event.which == RIGHT)
+      spin(event.which == RIGHT);
   });
 });
